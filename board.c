30562246867a9b0dc7b9ccf438afbe05cdc7d76d
Alexander Dunlap <alexander.dunlap@gmail.com>
2013-03-10 17:01:12 -0500

    Fixing stupid error in board distance function

--- board.c
+++ board.c
@@ -61,7 +61,7 @@ double score (othello_bd *bd) {
 }
 
 int distance_from_edge (int x, int y) {
-  return square_types[(x<<3)&y];
+  return square_types[(x<<3)^y];
   /*
   int result = x;
   int choices[3] = { y, 7-x, 7-y };
